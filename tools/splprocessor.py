1b5701af201f6c3934a76b33431ad4c369e4459f
Patricio <pmoracho@gmail.com>
2019-05-10 00:33:12 -0300

    work on splprocessor

--- tools/splprocessor.py
+++ tools/splprocessor.py
@@ -72,19 +72,47 @@ try:
 	import argparse
 	# from argparse import RawTextHelpFormatter
 	import sys
+	import time
+	import os
 
 	sys.path.append('.')
 	sys.path.append('..')
 
+	import logging
 	from openerm.Config import ProcessorConfig
 	from openerm.Config import ConfigLoadingException
 	from openerm.Utils import file_accessible
-
+	from watchdog.observers import Observer
+	from watchdog.events import LoggingEventHandler
+	from watchdog.events import FileSystemEventHandler
+	import psutil
 
 except ImportError as err:
 	modulename = err.args[0].partition("'")[-1].rpartition("'")[0]
 	print(_("No fue posible importar el modulo: %s") % modulename)
 	sys.exit(-1)
+
+
+def has_handle(fpath):
+
+	for proc in psutil.process_iter():
+		try:
+			for item in proc.open_files():
+				if fpath == item.path:
+					return True
+		except Exception:
+			pass
+
+	return False
+
+
+class NewSpoolHandler(FileSystemEventHandler):
+	
+	def on_created(self, event):
+		print("event type: {0} path: {1} {2}".format(	
+													event.event_type,
+													event.src_path,
+													"En uso" if has_handle(event.src_path) else ""))
 
 
 def init_argparse():
@@ -95,8 +123,13 @@ def init_argparse():
 										add_help=True,
 										formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=50)
 	)
+	opciones = {	"path": {
+								"type": str,
+								"action": "store",
+								"help": _("Path a monitorear")
+					},
 
-	opciones = {	"--config-file -f": {
+					"--config-file -f": {
 								"type": str,
 								"action": "store",
 								"dest": "configfile",
@@ -137,21 +170,39 @@ if __name__ == "__main__":
 		args.error(str(msg))
 
 	# if not args.configfile:
-	# 	print(_("Error: Debe definir el archivo de configuración del proceso").format(args.configfile))
-	# 	sys.exit(-1)
+	#	print(_("Error: Debe definir el archivo de configuración del proceso").format(args.configfile))
+	#	sys.exit(-1)
 	if not file_accessible(args.configfile, "r"):
 		print(_("Error: El archivo de configuración del proceso [{0}] no se ha encontrado o no es accesible para su lectura").format(args.configfile))
 		sys.exit(-1)
 
 	try:
 		cfg = ProcessorConfig(args.configfile)
-		print(cfg.dictionary)
+		# print(cfg.dictionary)
 
 	except ConfigLoadingException as ex:
-		print(ex.args[0])
-		print("\n".join([" - " + e for e in ex.args[1]]))
+		pass
+		# print(ex.args[0])                                                                                                            
+		# print("\n".join([" - " + e for e in ex.args[1]]))
 
 	else:
+
+		print("Monitoreando {0}".format(args.path))
+				
+		event_handler = NewSpoolHandler()
+
+		observer = Observer()
+		observer.schedule(event_handler, path=args.path, recursive=False)
+		observer.start()
+
+		try:
+			while True:
+				time.sleep(1)
+		except KeyboardInterrupt:
+			observer.stop()
+
+		observer.join()
+
 
 		print("")
 		sys.exit(-1)
